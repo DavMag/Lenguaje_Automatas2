cf83b2629dd15f71ec2a3862914e331b84712f31
DvMg <david.magallan@hotmail.com>
2018-05-04 13:54:37 -0500

    Creación clase Editor y Finalización de Lectura_VCI

---
+++ src/vci/Editor.java
@@ -0,0 +1,216 @@
+package vci;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author David Magallán Jiménez
+ */
+public class Editor {
+
+    JFrame ventana;
+    JTextArea notas;
+    File codigo_fuente;
+    File tabla_simbolos;
+    String path;
+    
+
+    public Editor() {
+
+        ventana = new JFrame("Creaci�n de VCI");
+
+        // Inicializa todos los elementos del menu
+        JMenuBar menu = new JMenuBar();
+
+        JMenu archivo = new JMenu("Archivo");
+        JMenu ayuda = new JMenu("Ayuda");
+
+        
+        JMenuItem vci = new JMenuItem("VCI");
+        
+        // Añade los elementos al menu
+        
+        archivo.add(vci);
+    
+
+        menu.add(archivo);
+        menu.add(ayuda);
+
+        // Añade la barra de menu a la ventana
+        ventana.setJMenuBar(menu);
+
+        // Cra un area de texto con scroll y lo añade a la ventana 
+        notas = new JTextArea();
+        JScrollPane scrollNotas = new JScrollPane(notas);
+        ventana.add(scrollNotas);
+
+        
+        
+         vci.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                vci();
+            }
+        });
+       
+        // Hace visible la ventana
+        ventana.setLocationRelativeTo(null);
+        ventana.setSize(500, 250);
+        ventana.setVisible(true);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void abrirArchivo() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        if (JFileChooser.APPROVE_OPTION == fileChooser.showOpenDialog(ventana)) {
+            File archivo = fileChooser.getSelectedFile();
+            FileReader lector = null;
+            try {
+                lector = new FileReader(archivo);
+                BufferedReader bfReader = new BufferedReader(lector);
+
+                String lineaFichero;
+                StringBuilder contenidoFichero = new StringBuilder();
+
+                // Recupera el contenido del fichero
+                while ((lineaFichero = bfReader.readLine()) != null) {
+                    contenidoFichero.append(lineaFichero);
+                    contenidoFichero.append("\n");
+                }
+                bfReader.close();
+                // Pone el contenido del fichero en el area de texto
+                notas.setText(contenidoFichero.toString());
+
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(Editor.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(Editor.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                try {
+                    lector.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(Editor.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+
+    public void guardarArchivo() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        if (JFileChooser.APPROVE_OPTION == fileChooser.showSaveDialog(ventana)) {
+            codigo_fuente = fileChooser.getSelectedFile();
+            FileWriter escritor = null;
+            try {
+                escritor = new FileWriter(codigo_fuente);
+                escritor.write(notas.getText());
+                path = codigo_fuente.getAbsolutePath();
+                System.out.print(path);
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(Editor.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(Editor.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                try {
+                    escritor.close();
+                   
+                } catch (IOException ex) {
+                    Logger.getLogger(Editor.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+    
+    /*
+    Método que se ejecuta al lanzar las excepciones de errores léxicos: Si lanza alguna se abre el mismo archivo y le dice que corrija
+    el error.
+    */
+    public void corregirArchivo()
+    {
+        
+            File archivo = codigo_fuente;//Archivo escrito por el usuario
+            FileReader lector = null;
+            try {
+                lector = new FileReader(archivo);
+                BufferedReader bfReader = new BufferedReader(lector);
+
+                String lineaFichero;
+                StringBuilder contenidoFichero = new StringBuilder();
+
+                // Recupera el contenido del fichero
+                while ((lineaFichero = bfReader.readLine()) != null) {
+                    contenidoFichero.append(lineaFichero);
+                    contenidoFichero.append("\n");
+                }
+                bfReader.close();
+                // Pone el contenido del fichero en el area de texto
+                notas.setText(contenidoFichero.toString());
+                
+
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(Editor.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(Editor.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                try {
+                
+                    lector.close();
+                    
+                } catch (IOException ex) {
+                    Logger.getLogger(Editor.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        
+    }
+    
+    /*
+    Método Compilar: Primero hará el análisis léxico y como vaya avanzando en el proyecto se  añadirán los siguientes pasos para la compilación
+    */
+       public void vci()
+       {
+    	 
+    	   System.out.println(notas.getText());
+    	  if(!(notas.getText().equals(""))) {
+    	   Lectura_VCI vci  = new Lectura_VCI(notas.getText());
+    	   vci.creacionVCI(); 
+    	   JOptionPane.showMessageDialog(null,vci.imprimirVCI(),"Vector C�digo Intermedio", JOptionPane.INFORMATION_MESSAGE);
+    	  }else {
+    		  JOptionPane.showMessageDialog(null,"No ha escrito ninguna linea de instrucci�n\n"
+    				  + "  Escriba una linea para continuar","Alertar", JOptionPane.WARNING_MESSAGE);  
+    	  }
+    	   
+       }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Editor bn = new Editor();
+    }
+
+}
--- src/vci/Lectura_VCI.java
+++ src/vci/Lectura_VCI.java
@@ -6,12 +6,18 @@ import java.util.StringTokenizer;
 
 public class Lectura_VCI {
 
-	ArrayList<String> tokens = new ArrayList<>(); //ArrayList que contendrá los elementos leidos
-	ArrayList<String> vci = new ArrayList<>(); //ArrayList que contendrá el vector de código intermedio
+	ArrayList<String> tokens = new ArrayList<>(); // ArrayList que contendrá los elementos leidos
 
-	Stack<String> operadores = new Stack<String>(); //Pila de operadores
+	ArrayList<String> vci = new ArrayList<>(); // ArrayList que contendrá el vector de código intermedio
 
-	//Método para leer los tokens de un código fuente y añadirlos al ArrayList tokens
+	Stack<String> operadores = new Stack<String>(); // Pila de operadores
+
+	public Lectura_VCI(String codigo) {
+		leerTokens(codigo);
+	}
+
+	// Método para leer los tokens de un código fuente y añadirlos al ArrayList
+	// tokens
 	private void leerTokens(String codigo) {
 		StringTokenizer stz = new StringTokenizer(codigo);
 
@@ -21,7 +27,7 @@ public class Lectura_VCI {
 
 	}
 
-	//Determinamos la prioridad
+	// Determinamos la prioridad
 	private int prioridad(String token) {
 		int prioridad = 0;
 		switch (token) {
@@ -62,20 +68,45 @@ public class Lectura_VCI {
 		return prioridad;
 	}
 
-	private void creacionVCI() {
+	protected void creacionVCI() {
+		System.out.println(tokens.size());
 		for (int i = 0; i < tokens.size(); i++) {
-			if (prioridad(tokens.get(i)) == -1) {
+			int prioridad = prioridad(tokens.get(i));
+
+			if (prioridad == -1 && !(tokens.get(i).equals(";"))) {
 				vci.add(tokens.get(i));
-			} else {
+			}else if(tokens.get(i).equals("("))
+			{
+				operadores.push(tokens.get(i));
+			}else if(tokens.get(i).equals(")"))
+			{
+				while(!(String.valueOf(operadores.peek()).equals("(")))
+				{
+					vci.add(operadores.pop());
+				}
+				
+				String basura = operadores.pop();
+			}
+			
+			else if (tokens.get(i).equals(";")) {
+
+				while (!(operadores.isEmpty())) {
+					vci.add(operadores.pop());
+				}
+
+			}
+
+			else {
 				int aux;
-				int prioridad = prioridad(tokens.get(i));
+
 				// Verifica que la pila no esté vacía
 				if (!(operadores.isEmpty())) {
 					aux = prioridad(String.valueOf(operadores.peek()));
-					if (prioridad >= aux) {
+					if (prioridad > aux) {
 						operadores.push(tokens.get(i));
 					} else {
 						vci.add(operadores.pop());
+						operadores.push(tokens.get(i));
 					}
 					// Si está vacía simplemente insertamos los valores en la pila
 				} else if (i == tokens.size() - 1) {
@@ -87,6 +118,16 @@ public class Lectura_VCI {
 			}
 
 		}
+	}
+
+	protected String imprimirVCI() {
+		String linea = "";
+
+		for (int i = 0; i < vci.size(); i++) {
+			linea += vci.get(i) + " ";
+		}
+
+		return linea;
 	}
 
 }
